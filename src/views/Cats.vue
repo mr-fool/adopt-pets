fabb06e395a90a4936772cc6fd12358be73bd93b
mr-fool <mr-fool@users.noreply.github.com>
2020-01-19 01:59:51 -0700

    Update Cats.vue

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,9 +3,7 @@
       <h1>Cats for Adoption</h1>
       <b-table striped hover :items="cats">
          <template slot="name" slot-scope="data">
-             <router-link :to="`/pets/${data.value}`">
-                {{ data.value }}
-            </router-link>
+             <router-link :to="`/pets/${data.value}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
